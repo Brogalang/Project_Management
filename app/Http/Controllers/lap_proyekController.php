a2e118929176af365abb0f0d1f88b67f073d03ee
Brogalang <brogalangsatria@gmail.com>
2022-11-15 10:04:50 +0700

    cleaning code

--- app/Http/Controllers/lap_proyekController.php
+++ app/Http/Controllers/lap_proyekController.php
@@ -30,26 +30,6 @@ class lap_proyekController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        $menus = Menu::where('parent_id', '=', 0)->get();
-        $jab = M_jabatan::all();
-        $div = M_divisi::distinct()->get(['id_divisi','nama_divisi']);
-        $dep = M_divisi::all();
-        return view('form.karycreate',compact('menus','jab','div','dep'));
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         if ($request->metode=='insertdata') {
@@ -67,49 +47,37 @@ class lap_proyekController extends Controller
                 'created_at' => date('Y-m-d h:i:s')
                 ]);
             return response()->json(['success' => 'Data saved successfully.']);
+        }else if ($request->metode=='editdata') {
+            $request->validate([
+                'prjprogres' => 'required',
+                'nmprogres' => 'required',
+            ]);
+            lap_proyek::where('id_progress', '=', $request->id_progres)->update([
+                'project_id' => $request->prjprogres,
+                'nama_progress' => $request->nmprogres,
+                'status_progress' => $request->statprogres,
+                'catatan_progress' => $request->catatan,
+                'tanggal_progress' => $request->tglprogres,
+                'updated_at' => date('Y-m-d h:i:s')
+            ]);
+            return response()->json(['success' => 'Data updated successfully.']);
         }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Product $product)
+    public function edit_lap(Request $request)
     {
-        $menus = Menu::where('parent_id', '=', 0)->get();
-        return view('datakaryawan.show',compact('product','menus'));
+        $listdata = lap_proyek::where('id_progress', '=', $request->id_progres)
+                                ->join('projects', 'projects.project_id', '=', 'projects_status.project_id')
+                                ->get();
+        return response()->json([
+            'listdata' => $listdata,
+        ]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($nik)
+    public function delete_lap(Request $request)
     {
-        print_r($nik);
-        // $kary = M_karyawan::where('nik', '=', $nik)->get();
-        // $jab = M_jabatan::all();
-        // $dep = M_divisi::all();
-        // $menus = Menu::where('parent_id', '=', 0)->get();
-        // return view('form.karyedit',compact('kary','menus','jab','dep'));
+        lap_proyek::where('id_progress', '=', $request->id_progres)->delete();
+        return response()->json(['success' => 'Data deleted successfully.']);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-
-    public function destroy($id_jabatan)
-    {
-        M_jabatan::where('id_jabatan', '=', $id_jabatan)->delete();
-        return redirect()->route('jabatan.index')
-                        ->with('success','Product deleted successfully');
-    }
 }
